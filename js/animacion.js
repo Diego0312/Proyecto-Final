5d5cc65d2c50742105c0f681dea21849cd6e9f98
Diego Peña <diegolajandro12@gmail.com>
2021-08-06 09:23:45 -0500

    Inclui interacciones

---
+++ js/animacion.js
@@ -0,0 +1,83 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".scroll_descripcion" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_video" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_descripcion1" ,{
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_fotografia" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+sr.reveal(".scroll_fotografia1" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_audiovisual" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_audiovisual1" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+sr.reveal(".scroll_ilustracion" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_ilustracion1" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_codigo" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_codigo1" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_equipo" ,{
+    duration:3000,
+    origin:'rigth',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_equipo1" ,{
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal(".scroll_formulario" ,{
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+});
